0c58268eeaa52bffa14419ad14231af7387aea17
Andy Fry <andyfry01@gmail.com>
2021-06-17 11:51:48 -0400

    kommit

--- functional-react/functional-react/src/App.js
+++ functional-react/functional-react/src/App.js
@@ -1,44 +1,41 @@
+// check out themes at:
+// https://github.com/PrismJS/prism-themes
+
 import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import CodeMirror from "react-codemirror";
-import 'codemirror/lib/codemirror.css'
-import {transform}  from "@babel/standalone";
+import "prismjs"
+import "prismjs/components/prism-jsx.min";
+import "./theme.css"
+import PrismCode from "react-prism";
 
-const test = 'const Calculator = props => { return <div>{props.number + props.children}</div>; };'
-// console.log(eval(test))
-
-// const Demo = props => {
-//   const AddTen = <Calculator number={10}>{props.numbers[0]}</Calculator>
-//   const AddTwenty = <Calculator number={20}>{props.numbers[1]}</Calculator>
-
-//   return (
-//     <div>
-//       {AddTen}
-//       {AddTwenty}
-//     </div>
-//   )
-// }
-
-// const Example = () => {
-//   const numbers = [10, 20]
-
-//   return <Demo numbers={numbers} />
-// '
+const example = [
+  "const Demo = props => {",
+  " const AddTen = <Calculator number={10}>{props.numbers[0]}</Calculator>",
+  " const AddTwenty = <Calculator number={20}>{props.numbers[1]}</Calculator>",
+  " return (",
+  "    <div>",
+  "      {AddTen}",
+  "      {AddTwenty}",
+  "    </div>",
+  " )",
+  "}",
+  "\n",
+  "const Example = () => {",
+  " const numbers = [10, 20]",
+  "\n",
+  " return <Demo numbers={numbers} />",
+  "}"
+].join('\n')
 
 const App = () => {
-  const [value, setValue] = useState(test)
-  // console.log(transform);
-  var output = transform('const test = () => (return <div>hello world!</div>)', { presets: ["react"] });
+  const [value, setValue] = useState(example)
 
-  console.log(output)
   return (
     <div className="App">
-      Heloo
-      <CodeMirror
-        value={value}
-        onChange={setValue}
-      />
+      <pre>
+        <PrismCode className="language-jsx">
+          {example}
+        </PrismCode>
+      </pre>
     </div>
   );
 }
